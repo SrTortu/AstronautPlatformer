418b46f075ccf7bfaa9ca1530cf32fd58dbdb028
SrTortu <santiagogc0828@gmail.com>
2025-01-23 18:01:03 +0100

    Se añade alerta visual y sonora para cuando el player no tenga suficiente combustible para hacer un salto

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -12,23 +12,42 @@ public class UIController : MonoBehaviour
 	[SerializeField] private Jetpack _jetpack;
 	[SerializeField] private Slider _energySlider;
 	[SerializeField] private TextMeshProUGUI _textSlider;
-	#endregion
+	[SerializeField] private TextMeshProUGUI _energyAlert;
+	[SerializeField] private AudioSource _audioSource;
+	[SerializeField] private AudioClip _noEnergySound;
+	
+    #endregion
 
-	#region Unity Callbacks
+    #region Unity Callbacks
 
-
+    private void Start()
+    {
+		_audioSource = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void Update()
     {
 		_energySlider.value = _jetpack.Energy;
 		_textSlider.text = ((int)_jetpack.transform.position.y).ToString();
 
+		if(!_jetpack.Flying && Input.GetKeyDown(KeyCode.Space))
+        {
+			_audioSource.PlayOneShot(_noEnergySound);
+			StartCoroutine(ShowAlert());
+        }	
+
 	}
 	#endregion
 
 	#region Public Methods
+	System.Collections.IEnumerator ShowAlert()
+	{
+		_energyAlert.gameObject.SetActive(true); // Mostrar alerta
+		yield return new WaitForSeconds(1f); // Esperar 1 segundo
+		_energyAlert.gameObject.SetActive(false); // Ocultar alerta
+	}
 	#endregion
 
 	#region Private Methods
-	#endregion   
+	#endregion
 }
