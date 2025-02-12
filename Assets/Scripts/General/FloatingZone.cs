617c545a6621dedc3432a3a1c1fffcc46927495b
SrTortu <santiagogc0828@gmail.com>
2025-02-12 19:33:22 +0100

    Fixed script : end.cs, now called FloatingZone.cs

--- Assets/Scripts/General/FloatingZone.cs
+++ Assets/Scripts/General/FloatingZone.cs
@@ -2,29 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class end : MonoBehaviour
+public class FloatingZone : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float endForce;
+    [SerializeField] private float floatingForce;
     private Rigidbody2D _playerRB;
     private ItemSpawner spawner;
+    private void Start()
+    {
+        floatingForce = 19000;
+    }
     private void Update()
     {
         if (_playerRB != null)
-            _playerRB.AddForce(Vector2.up * endForce * Time.deltaTime);
+        {
+            _playerRB.AddForce(Vector2.up * floatingForce * Time.deltaTime);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            _playerRB = collision.gameObject.GetComponent<Rigidbody2D>();
+            _playerRB = collision.gameObject.GetComponent<Rigidbody2D>();   
             if(spawner != null)
             {
                 spawner = FindObjectOfType<ItemSpawner>();
                 spawner.enabled = false;
             }
-
-
         }
     }
 }
