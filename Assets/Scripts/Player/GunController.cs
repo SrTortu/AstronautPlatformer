5575fff0f925d843a427e706e5347afa329659f2
SrTortu <santiagogc0828@gmail.com>
2025-02-13 20:32:17 +0100

    Fixed Script: Gun Controller Delete Script : PlayerCollisionController

--- Assets/Scripts/Player/GunController.cs
+++ Assets/Scripts/Player/GunController.cs
@@ -1,55 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-    [SerializeField]private Transform shootPoint;
     [SerializeField] private HookTrigger _hookTrigger;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip _audioClip;
+
     private void Start()
     {
-        shootPoint = transform.Find("shoot_point");
         _hookTrigger = GetComponentInChildren<HookTrigger>();
-
     }
-    public void Aim(Vector3 MousePosition)
+
+    public void Aim(Vector3 mousePosition)
     {
-        Vector3 direction = MousePosition - transform.position;
-
-     
-
+        Vector3 direction = mousePosition - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         float rotationAmount;
-
         // Invert gun relative to mouse
         Vector3 localScale = transform.localScale;
-       // localScale.y = angle > 90 || angle < -90 ? -1 : 1;
         localScale.x = angle > 90 || angle < -90 ? -1 : 1;
         rotationAmount = angle > 90 || angle < -90 ? 180 : 0;
-        angle = angle > 90 || angle < -90 ? angle*-1 : angle*1;
+        angle = angle > 90 || angle < -90 ? angle * -1 : angle * 1;
         transform.rotation = Quaternion.Euler(rotationAmount, 0, angle);
         transform.localScale = localScale;
-        
     }
 
-    public void FlipShootPoint (float angle)
-    {
-        
-        //shootPoint.rotation = Quaternion.Euler(0, angle, 0);
-        //Debug.Log($"entre shoot poitn $angle"+ angle);
-    }
-    public void shoot (Vector3 MousePosition)
-    {
-        _hookTrigger.launchHook(MousePosition);
 
-        if(_hookTrigger.isHooked)
+    public void Shoot(Vector3 mousePosition)
+    {
+        _hookTrigger.launchHook(mousePosition);
+
+        if (_hookTrigger.isHooked)
             _audioSource.PlayOneShot(_audioClip);
-        
-        
     }
-    public void DetachHook ()
+
+    public void DetachHook()
     {
         _hookTrigger.DetachHook();
     }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,112 +1,91 @@
 using UnityEngine;
-using System;
-using UnityEngine.Rendering.Universal;
 using System.Collections;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(SpriteRenderer))]
-
 public class Player : MonoBehaviour
 {
-	#region Properties
-	#endregion
-	public enum Direction
-	{
-		Left,
-		Right
-	}
+    public enum Direction
+    {
+        Left,
+        Right
+    }
 
-	#region Fields
-	private bool _walking;
-	
+    [SerializeField] private Jetpack _jetpack;
+    [SerializeField] private float _moveSpeed;
+    private Animator _anim;
+    private Rigidbody2D _targetRB;
+    public GunController PlayerGun;
+    public bool isDamage;
+    private bool _isWalking;
 
-	[SerializeField] private Jetpack _jetpack;
-	[SerializeField] private float _moveSpeed;
-	private Animator _anim;
-	private Rigidbody2D _targetRB;
-	public GunController PlayerGun;
-	public bool isOnGround;
-	public bool isDamage;
-
-   
-    #endregion
-
-    #region Unity Callbacks
     private void Awake()
-	{
-		isDamage = false;
-		_anim = GetComponent<Animator>();
-		_targetRB = GetComponent<Rigidbody2D>();
-		PlayerGun = GetComponentInChildren<GunController>();
-		
-	}
+    {
+        isDamage = false;
+        _anim = GetComponent<Animator>();
+        _targetRB = GetComponent<Rigidbody2D>();
+        PlayerGun = GetComponentInChildren<GunController>();
+    }
 
-	public void Walk(Direction walkDirection)
-	{
-		if (_jetpack.Flying) //Si esta volando no puede volar
-			return;
-		if (walkDirection == Direction.Left)
-			_targetRB.AddForce(Vector2.left * _moveSpeed, ForceMode2D.Impulse);
-		if (walkDirection == Direction.Right)
+    void Update()
+    {
+        _anim.SetBool("Flying", _jetpack.Flying);
+        _anim.SetBool("Walking", _isWalking);
+        _anim.SetBool("Damage", isDamage);
+    }
+
+    public void Walk(Direction walkDirection)
+    {
+        if (_jetpack.Flying) //Si esta volando no puede volar
+            return;
+        if (walkDirection == Direction.Left)
+            _targetRB.AddForce(Vector2.left * _moveSpeed, ForceMode2D.Impulse);
+        if (walkDirection == Direction.Right)
         {
-			_targetRB.AddForce(Vector2.right * _moveSpeed, ForceMode2D.Impulse);
-
+            _targetRB.AddForce(Vector2.right * _moveSpeed, ForceMode2D.Impulse);
         }
-	}
-
-	public void WalkOn()
-	{
-		_walking = true;
-	}
-	public void WalkOff()
-	{
-		_walking = false;
-	}
-	public void DagameOn ()
-    {
-		StartCoroutine (MakeDamage());
     }
-	
-	public void Flip(Direction direction) //Hace que el player cambie visualmente de direccion
+
+    public void WalkOn()
     {
-		
-		Vector3 scale = transform.localScale;
-		if (direction == Direction.Left)
+        _isWalking = true;
+    }
+
+    public void WalkOff()
+    {
+        _isWalking = false;
+    }
+
+    public void DagameOn()
+    {
+        StartCoroutine(MakeDamage());
+    }
+
+    public void Flip(Direction direction) //Hace que el player cambie visualmente de direccion
+    {
+        Vector3 scale = transform.localScale;
+        if (direction == Direction.Left)
         {
-			scale.x = -1;
-			PlayerGun.FlipShootPoint(0f);
+            scale.x = -1;
         }
-		else
+        else
         {
-			scale.x = 1;
-			PlayerGun.FlipShootPoint(180f);
-		}
-		transform.localScale = scale;
-    }
-	public void Shoot(Vector3 mouseDirection)
-	{
-		
-		PlayerGun.shoot(mouseDirection);
-	}
-	IEnumerator MakeDamage ()
-    {
-		isDamage = true;
-		yield return new WaitForSeconds(1f);
-		isDamage = false;
-    }
-	// Update is called once per frame
-	void Update()
-    {
-		_anim.SetBool("Flying", _jetpack.Flying);
-		_anim.SetBool("Walking", _walking);
-		_anim.SetBool("Damage", isDamage);
-    }
-	#endregion
+            scale.x = 1;
+        }
 
-	#region Public Methods
-	#endregion
+        transform.localScale = scale;
+    }
 
-	#region Private Methods
-	#endregion   
+    public void Shoot(Vector3 mouseDirection)
+    {
+        PlayerGun.Shoot(mouseDirection);
+    }
+
+    IEnumerator MakeDamage()
+    {
+        isDamage = true;
+        yield return new WaitForSeconds(1f);
+        isDamage = false;
+    }
 }
