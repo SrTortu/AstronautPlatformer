1793e7e657a91024e3cb5c297d1b7ae7f54673bf
SrTortu <santiagogc0828@gmail.com>
2025-02-13 20:16:35 +0100

    Fixed Script: PlatformMobile

--- Assets/Scripts/Platform/PlatformMobile.cs
+++ Assets/Scripts/Platform/PlatformMobile.cs
@@ -1,15 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformMobile : MonoBehaviour
 {
-    [SerializeField]private float _speed;
+    [SerializeField] private float _speed;
     private Vector2 _pointA;
     private Vector2 _pointB;
     private Vector2 _target;
     private bool _isActive;
-    
+
     void Start()
     {
         _pointA = new Vector2(-5, this.transform.position.y);
@@ -20,11 +18,11 @@ public class PlatformMobile : MonoBehaviour
 
     void Update()
     {
-        if(_isActive)
-        { 
+        if (_isActive)
+        {
             transform.position = Vector2.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
 
-            if(Vector2.Distance(transform.position, _target) < 0.1f)
+            if (Vector2.Distance(transform.position, _target) < 0.1f)
             {
                 _target = (_target == _pointA) ? _pointB : _pointA;
             }
@@ -33,7 +31,7 @@ public class PlatformMobile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       if(this.CompareTag("EarthPlatform"))
+        if (this.CompareTag("EarthPlatform"))
         {
             if (collision.gameObject.CompareTag("Player"))
             {
@@ -41,6 +39,7 @@ public class PlatformMobile : MonoBehaviour
             }
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (this.CompareTag("EarthPlatform"))
@@ -51,6 +50,7 @@ public class PlatformMobile : MonoBehaviour
             }
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("PlayerChunk"))
@@ -58,6 +58,7 @@ public class PlatformMobile : MonoBehaviour
             _isActive = true;
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("PlayerChunk"))
